773ce507d29a2acce3af29c9238d56d882184af0
Whoisbezo.fx <bezzo19@gmx.de>
2025-11-30 02:16:20 +0100

    App.js aktualisieren

--- src/App.js
+++ src/App.js
@@ -5,8 +5,6 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-        </p>
       </header>
     </div>
   );
