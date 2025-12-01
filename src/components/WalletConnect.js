02403332137db4a44b7884485d743935e4115dcf
Whoisbezo.fx <bezzo19@gmx.de>
2025-12-01 13:58:51 +0100

    Update WalletConnect.js

--- src/components/WalletConnect.js
+++ src/components/WalletConnect.js
@@ -2,6 +2,10 @@ import React, { useState, useEffect } from 'react';
 import { Connection, LAMPORTS_PER_SOL, PublicKey } from '@solana/web3.js';
 import './WalletConnect.css';
 
+import phantom_icon from './phantom_icon.png';
+import solflare_icon from './solflare_icon.png';
+
+
 const WalletConnect = () => {
   const [isConnected, setIsConnected] = useState(false);
   const [walletAddress, setWalletAddress] = useState('');
