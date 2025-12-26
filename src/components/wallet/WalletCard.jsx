b10e50532d2e93a1236631458dd15d9082da4b9a
Whoisbezo.fx <bezzo19@gmx.de>
2025-12-26 17:42:32 +0100

    WalletCard.jsx erstellen

---
+++ src/components/wallet/WalletCard.jsx
@@ -0,0 +1,12 @@
+import "./WalletCard.css";
+
+export default function WalletCard() {
+  return (
+    <div className="wallet-card">
+      <div className="wallet-content">
+        <span className="wallet-title">WALLET</span>
+        <span className="wallet-balance">$3.400,23</span>
+      </div>
+    </div>
+  );
+}
